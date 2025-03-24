5363a48ae34ce82de1941d354b941a46d6823c91
Mykhailo Loniak <loniakmykhail@gmail.com>
2025-03-24 22:55:36 +0100

    fix

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ const server = http.createServer(app);
 const PORT = process.env.PORT || 3005;
 
 const corsOptions = {
-  origin: '*',
+  origin: process.env.CLIENT_ORIGIN,
   credentials: true,
 };
 
